790ddec43fc03feb415592266ea9ac3833b1d73f
Govind <govind@Govinds-Mac-mini.local>
2026-02-28 23:33:48 -0600

    Update social links: Facebook -> kgf4bharat, X -> kgfbharat

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -14,7 +14,7 @@ const quickLinks = [
 const socialLinks = [
     {
         name: "Facebook",
-        href: "#",
+        href: "https://www.facebook.com/kgf4bharat/",
         icon: (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
@@ -23,7 +23,7 @@ const socialLinks = [
     },
     {
         name: "X (Twitter)",
-        href: "#",
+        href: "https://x.com/kgfbharat",
         icon: (
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M4 4l11.733 16h4.267l-11.733 -16h-4.267z" />
